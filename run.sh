eeb209f4d981a5657ed0ee2d5ac51839d3ba6d8d
Joao Guimaraes <joao@linux.com.uy>
2019-05-09 20:35:38 -0300

    New Push

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -ex
+
+JAVA_BIN=/usr/lib/jvm/java-1.8.0-openjdk-amd64/jre/bin/java
+JENKINS_HOME=
+
+${JAVA_BIN} -jar /home/jenkins/swarm-client-${SWARM_CLIENT_VERSION}.jar -master ${MASTER_URL} -username ${MASTER_USER} -passwordEnvVariable MASTER_PASSVAR -labels JDK8 -executors ${SLAVE_EXES:-2} -fsroot /
